e4ff6da144900d81b1f6c713f11f3f23faaa9397
DiogoCC7 <dassuncao1234@gmail.com>
2024-10-12 23:35:10 +0100

    update: Fish Shell

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,13 @@
 set fish_greeting
+set fish_vi_key_bindings
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# Environment Variables
+set -gx TERM xterm-256color
+set -x FZF_DEFAULT_OPTS "--preview 'bat --color=always {}'"
 
 #starship init fish | source
 oh-my-posh init fish --config $HOME/.config/ohmyposh/theme.json | source
